443fa52a582e207161747c9b7cda95f05013a7b6
mhowlader <mhowlader@stuy.edu>
2018-01-05 09:22:36 -0500

    Added chessboard, ppiece, and rook

---
+++ ChessPiece.java
@@ -0,0 +1,5 @@
+public abstract class ChessPiece {
+  public ChessPiece(char c);
+  public String color;
+  public String toString();
+  }
---
+++ Rook.java
@@ -0,0 +1,8 @@
+public class Rook extends ChessPiece {
+  public Rook(char c) {
+
+  }
+  public String toString() {
+    return color + "R";
+  }
+}
