715cd6a235de2e0420b074cec7ca846cd279199d
Kevin Feng <kfeng@gmail.com>
2018-01-08 04:35:31 -0500

    added colored labels to board

--- stuff/Board.java
+++ stuff/Board.java
@@ -1,27 +1,34 @@
 public class Board {
     private static String[][] board;
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	
     public Board() {
-	board = new String[8][8];
+	board = new String[9][9];
     }
-    private String set( int r, int c, String newVal )
+    
+	private String set( int r, int c, String newVal )
     {
 	String retVal = board[r][c];
 	board[r][c] = newVal;
 	return retVal;
     }
+   
     public String toString()
     {
 	String foo = "";
-	for( int i =0; i < 8; i++ ) {
+	for( int i = 0; i < 9; i++ ) {
 	    foo += "| ";
-	    for( int j=0; j < 8; j++ ) {
+	    for( int j= 0; j < 9; j++ ) {
 		foo += board[i][j] + " "; //get(i+1,j+1)
 	    }
 	    foo += "|\n";
 	}
-
 	return foo;
     }
+	
+	
     public static void main( String[] Args) {
 	Board m1 = new Board();
 	for(int x = 0; x != 8; x++) {
@@ -61,6 +68,28 @@ public class Board {
 	m1.set(6,5,"P");
 	m1.set(6,6,"P");
 	m1.set(6,7,"P");
+	//set labels
+	m1.set(8,0,ANSI_BLUE + "A" + ANSI_RESET);
+	m1.set(8,1,ANSI_BLUE + "B" + ANSI_RESET);
+	m1.set(8,2,ANSI_BLUE + "C" + ANSI_RESET);
+	m1.set(8,3,ANSI_BLUE + "D" + ANSI_RESET);
+	m1.set(8,4,ANSI_BLUE + "E" + ANSI_RESET);
+	m1.set(8,5,ANSI_BLUE + "F" + ANSI_RESET);
+	m1.set(8,6,ANSI_BLUE + "G" + ANSI_RESET);
+	m1.set(8,7,ANSI_BLUE + "H" + ANSI_RESET);
+	m1.set(8,8,ANSI_BLUE + " " + ANSI_RESET);
+	
+	
+	m1.set(0,8,ANSI_BLUE + "1" + ANSI_RESET);
+	m1.set(1,8,ANSI_BLUE + "2" + ANSI_RESET);
+	m1.set(2,8,ANSI_BLUE + "3" + ANSI_RESET);
+	m1.set(3,8,ANSI_BLUE + "4" + ANSI_RESET);
+	m1.set(4,8,ANSI_BLUE + "5" + ANSI_RESET);
+	m1.set(5,8,ANSI_BLUE + "6" + ANSI_RESET);
+	m1.set(6,8,ANSI_BLUE + "7" + ANSI_RESET);
+	m1.set(7,8,ANSI_BLUE + "8" + ANSI_RESET);
+	
+	
 	System.out.println(m1);
     }
 }
