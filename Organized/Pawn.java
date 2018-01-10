dd3ec36379750128034ec5da47da040a0af04f92
keevon <kfeng@stuy.edu>
2018-01-10 09:35:01 -0500

    working on validmoves feature

--- Organized/Pawn.java
+++ Organized/Pawn.java
@@ -1,16 +1,20 @@
 public class Pawn extends ChessPiece {
     int row,col;
-    public Pawn(int x,int y) {
-	col = x;
-	row = y;
-    }
-    public boolean validMoves() {
-	if (row != (row +1)) {
-	    throw new IllegalArgumentException("Invalid move");
+    public Pawn(){
+	row = 99;
+	col = 99;	
 	}
-	else {
-	    return true; 
+    public int[] validMoves() {
+	int[] returnArray = new int[50];
+	if (col == 2 || col == 7){
+		while (
+		if (row != (row +1)) {
+		    throw new IllegalArgumentException("Invalid move");
+		}
+		else {
+		    return true; 
+		}
+	    }
 	}
-    }
 }
     
