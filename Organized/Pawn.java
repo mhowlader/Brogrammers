f66b11d497726731e503f531b7a124e58ceba9f0
keevon <kfeng@stuy.edu>
2018-01-11 05:57:25 -0500

    developing validmoves method for all the pieces

--- Organized/Pawn.java
+++ Organized/Pawn.java
@@ -4,17 +4,38 @@ public class Pawn extends ChessPiece {
 	row = 99;
 	col = 99;	
 	}
-    public int[] validMoves() {
-	int[] returnArray = new int[50];
-	if (col == 2 || col == 7){
-		while (
-		if (row != (row +1)) {
-		    throw new IllegalArgumentException("Invalid move");
+	
+	public boolean canPawnTake(){ // bottom player
+		if (enemy piece is in : (row + 1) * 10 + colPawn + 1) || (row + 1) * 10 + colPawn - 1){
+			return true;
+		}else{
+			return false;
 		}
-		else {
-		    return true; 
+	}
+	
+	public boolean canPawnTake(){ // top player
+		if (enemy piece is in : (row - 1) * 10 + colPawn + 1) || (row - 1) * 10 + colPawn - 1){
+			return true;
+		}else{
+			return false;
 		}
-	    }
+	}
+    public int[] validMoves(rowPawn, colPawn) {
+		int[] possibleMoves = new int[50];
+		if (rowPawn == 2 || rowPawn == 7){
+			possibleMoves[1] = 4 * 10 + colPawn; // bottom side pawns can advance 2 up
+			possibleMoves[2] = 5 * 10 + colPawn;  //top side pawns can advance 2 down
+		}
+		if (){ //pawn is bottom players, then
+			possibleMoves[3] = (rowPawn + 1) * 10 + colPawn;
+		}
+		if (){ //pawn is top players, then
+			possibleMoves[4] = (rowPawn - 1) * 10 + colPawn;
+		}
+		if (){ //other player's pawn is in a diagonal
+			
+		}
 	}
 }
+
     
