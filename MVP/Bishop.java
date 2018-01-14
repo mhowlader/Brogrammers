9c377aeea0523fc5ef7345702cdae66c4c4c7504
Alan Wang <awang15@stuy.edu>
2018-01-14 16:56:40 -0500

    WIP

--- MVP/Bishop.java
+++ MVP/Bishop.java
@@ -5,20 +5,9 @@ public class Bishop extends ChessPiece {
 	col = x;
 	row = y;
     }
-    public void fillValidMoves() {
-		validMoves = new String[70];
-		for (int i = 0; i < 8; i++){
-			validMoves[(i * 4)] = ((row + i + 1)* 10 + (col + i + 1)); //row is in the tens value, col is in the ones val
-			validMoves[(i * 4) + 1] = ((row + i + 1)* 10 + (col - i - 1));
-			validMoves[(i * 4) + 2] = ((row - i - 1) * 10 + (col + i + 1));
-			validMoves[(i * 4) + 3] = ((row - i - 1)* 10 + (col - i - 1));
-		}
-	}
-	
-	public String[][] getValidMoves(){
-		return validMoves;
-	}
-	
+    public String[] validMoves() {
+	return null;
+    }
     public String toString() {
 	return "B";
     }
