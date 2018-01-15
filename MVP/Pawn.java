5b0d5e232d625556ea4cc6e3a58d08e40494493e
mhowlader <mhowlader@stuy.edu>
2018-01-15 01:34:18 -0500

    restrictions pawn.java

--- MVP/Pawn.java
+++ MVP/Pawn.java
@@ -9,6 +9,41 @@ public class Pawn extends ChessPiece {
     }
     public ArrayList<int[]> fillValidMoves() {
 		validMoves = new ArrayList<int[]>();
+
+        if (color.equals("White") ) {
+            if (!ChessBoard.isPieceOnSquare(row+1,col)) {
+                validMoves.add(new int[]{row+1,col});
+                if (row==1 && !ChessBoard.isPieceOnSquare(row+2,col)) {
+                    validMoves.add(new int[]{row+2,col});
+                }
+            }
+
+            if (!ChessBoard.squareExists(row+1,col+1) && ChessBoard.isPieceOnSquare(row+1,col+1) && !sameColor(ChessBoard.getPiece(row+1,col+1))) {
+                validMoves.add(new int[]{row+1,col+1});
+            }
+
+            if (!ChessBoard.squareExists(row+1,col-1) && ChessBoard.isPieceOnSquare(row+1,col-1) && !sameColor(ChessBoard.getPiece(row+1,col-1))) {
+                validMoves.add(new int[]{row+1,col-1});
+            }
+        }
+
+        if (color.equals("Black") ) {
+            if (!ChessBoard.isPieceOnSquare(row-1,col)) {
+                validMoves.add(new int[]{row-1,col});
+                if (row==1 && !ChessBoard.isPieceOnSquare(row-2,col)) {
+                    validMoves.add(new int[]{row-2,col});
+                }
+            }
+
+            if (!ChessBoard.squareExists(row-1,col-1) && ChessBoard.isPieceOnSquare(row-1,col-1) && !sameColor(ChessBoard.getPiece(row-1,col-1))) {
+                validMoves.add(new int[]{row-1,col-1});
+            }
+
+            if (!ChessBoard.squareExists(row-1,col+1) && ChessBoard.isPieceOnSquare(row-1,col+1) && !sameColor(ChessBoard.getPiece(row-1,col+1))) {
+                validMoves.add(new int[]{row-1,col+1});
+            }
+        }
+
         return validMoves;
     }
     public String toString() {
