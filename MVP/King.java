3a59873fa0fb77e9b882e71c5ed03246c38f2136
mhowlader <mhowlader@stuy.edu>
2018-01-15 00:57:15 -0500

    added king restictions

--- MVP/King.java
+++ MVP/King.java
@@ -9,6 +9,26 @@ public class King extends ChessPiece {
     }
     public ArrayList<int[]> fillValidMoves() {
 		validMoves = new ArrayList<int[]>();
+        int[][] tempCol= new int[][]{
+            {row+1,col},
+            {row+1,col+1},
+            {row+1,col-1},
+            {row,col-1},
+            {row,col+1},
+            {row-1,col-1},
+            {row-1,col},
+            {row-1,col+1}
+        };
+
+        for (int[] b:tempCol) {
+            int r=b[0]; //row of tempCol
+            int c=b[1]; //column of tempCol
+            if (ChessBoard.squareExists(r,c)) { //checks if the square is on the chessboard
+              if (!ChessBoard.isPieceOnSquare(r,c) || !sameColor(ChessBoard.getPiece(r,c)) ) { //it's either an empty square or a square with an enemy piece
+                  validMoves.add(b); //add itto validmoves if so
+              }
+            }
+        }
         return validMoves;
     }
     public String toString() {
