9b518e48d1249d2f2ae6115dc6eb4d0554054c9b
keevon <kfeng@stuy.edu>
2018-01-15 04:51:02 -0500

    added pawn promotion functionality

--- MVP/Pawn.java
+++ MVP/Pawn.java
@@ -1,8 +1,13 @@
-
+import java.io.*;
+import java.util.*;
 import java.util.ArrayList;
 public class Pawn extends ChessPiece {
     //private static int row,col;
     public static ArrayList<int[]> validMoves;
+	private static InputStreamReader isr;
+	private static BufferedReader in;
+	
+	
     public Pawn(int x,int y, int player) { //use coor index at 0
         super(x,y,player);
         fillValidMoves();
@@ -47,7 +52,34 @@ public class Pawn extends ChessPiece {
 
         return validMoves;
     }
+	
+	public void upgradePawn(int row, int col, int player){ 
+		isr = new InputStreamReader(System.in);
+		in = new BufferedReader(isr);
+		String userInput;
+		
+		String errorMessage = "Invalid piece name, available pieces : Bishop, Knight, Queen, Rook";
+		System.out.println("What piece do you want to promote your pawn into? Enter full name of the piece");
+		try {
+			userInput = in.readLine();
+			//checks userinput for a valid choice
+			if(userInput.equals("Bishop")){
+				ChessBoard.setPieceOnBoard(row, col, new Bishop(row, col, player)); 
+			} else if(userInput.equals("Knight")){
+				ChessBoard.setPieceOnBoard(row, col, new Knight(row, col, player));
+			} else if (userInput.equals("Queen")){
+				ChessBoard.setPieceOnBoard(row, col, new Queen(row, col, player));
+			} else if (userInput.equals("Rook")){
+				ChessBoard.setPieceOnBoard(row, col, new Rook(row, col, player));
+			} else {
+				System.out.println(errorMessage);
+			}
+			} catch (IOException e){
+			System.out.println(errorMessage);
+		}
+	}
+	
     public String toString() {
-	return "P";
+		return "P";
     }
 }
