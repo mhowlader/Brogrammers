bcf78cb7a6320a49290b63a4f4c2b12f93be7983
yitian feng <kfeng@stuy.edu>
2018-01-16 19:59:39 -0500

    changed options from checkers to chessAI

--- Organized/playGame.java
+++ Organized/playGame.java
@@ -1,5 +1,5 @@
-import java.io.*; 
-import java.util.*; 
+import java.io.*;
+import java.util.*;
 
 public class playGame {
 	private boolean gameChosen;
@@ -8,65 +8,65 @@ public class playGame {
 	private BufferedReader in;
 	private String player1Name;
 	private String player2Name;
-	
-	public playGame() { 
+
+	public playGame() {
 		gameChosen = false;
 		isr = new InputStreamReader(System.in);
 		in = new BufferedReader(isr);
 		player1Name = "default";
 		player2Name = "default";
-		newGame(); 
+		newGame();
 	}
 
-	public void newGame() { 
+	public void newGame() {
 		String s;
 		s = "Welcome! \n";
- 
-		s += "\nChoose your game: \n";
-		s += "\t1: Chess\n";
-		s += "\t2: Checkers\n";
+
+		s += "\nChoose your gamemode: \n";
+		s += "\t1: Two Players\n";
+		s += "\t2: Single Player\n";
 		System.out.print(s);
- 
-		//This section was changed to handle people putting in weird values 
+
+		//This section was changed to handle people putting in weird values
 
 		String badValue = "\n Enter a number between 1 and 2 \n \n";
- 
+
 		while (!gameChosen) {
-			try { 
+			try {
 				System.out.print("Selection: ");
 				gameSelection = Integer.parseInt(in.readLine());
 				if (gameSelection >= 1 && gameSelection <= 2) {
 					gameChosen = true;
-				} else { 
+				} else {
 					System.out.print(badValue);
-				} 
+				}
 			} catch (IOException e) {
 				System.out.print(badValue);
 			} catch (NumberFormatException e) {
 				System.out.print(badValue);
-			} 
-		} 
+			}
+		}
  //get names
 		s = "player 1 name: ";
 		System.out.print(s);
 
-		try { 
+		try {
 			player1Name = in.readLine();
 		} catch (IOException e) {
-		} 
+		}
 
 		s = "player 2 name: ";
 		System.out.print(s);
 
-		try { 
+		try {
 			player2Name = in.readLine();
 		} catch (IOException e) {
-		} 
+		}
 
- 
-		
-	}//end newGame() 
-	
+
+
+	}
+
 	//
 	public String getPlayer1Name(){
 		return player1Name;
@@ -74,23 +74,23 @@ public class playGame {
 
 	public String getPlayer2Name(){
 		return player2Name;
-	}	
-	
+	}
+
 	public static void main(String[] args) {
 		//to end game, make game = 0
 		playGame ingame = new playGame();
-		
-		while (gameSelection == 1) { //run chess
+
+		while (gameSelection == 1) { //run chess 2 players here
 			System.out.println("running test chess");
 			System.out.println(Board.getBoard());
  			break;
-		} 
-		
-		while (gameSelection == 2) { //run checkers
-			System.out.println("running test checkers");
+		}
+
+		while (gameSelection == 2) { //run chess single player here
+			System.out.println("running test chess2");
 			break;
 		}
- 
-		System.out.println("Thy game doth be over.");
-	}//end main 
+
+		System.out.println("Thank you for playing!");
+	}//end main
 }
